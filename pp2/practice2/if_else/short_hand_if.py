bb38806a71f12c01bf71ac469e798077b3072e8d
dulat <dulattoktar@icloud.com>
2026-02-02 16:38:16 +0500

    Practice_PP2

---
+++ pp2/practice2/if_else/short_hand_if.py
@@ -0,0 +1,43 @@
+#1
+age = 18
+print("Adult" if age >= 18 else "Child")
+
+#2
+x = -3
+print("Positive" if x > 0 else "Not positive")
+
+#3
+n = 5
+print("Even" if n % 2 == 0 else "Odd")
+
+#4
+password = "1234"
+print("Access granted" if password == "1234" else "Access denied")
+
+#5
+temp = 30
+print("Hot" if temp > 25 else "Cold")
+
+
+#6
+temp = 20
+print("Hot" if temp > 25 else "Cold")
+
+#7
+score = 75
+print("Pass" if score >= 60 else "Fail")
+
+
+#8
+day = "Saturday"
+print("Weekend" if day == "Sunday" else "Weekday")
+
+#9
+a = 15
+print("Positive" if a > 0 else "Negative")
+
+
+#10
+a = 10
+b = 7
+print("a is bigger" if a > b else "b is bigger or equal")
